7b071ce5397b991ef4eb0870bebd2be1067b7265
ahmed-ali-bt <68769286+ahmed-ali-bt@users.noreply.github.com>
2020-11-29 12:08:37 +0500

    Project Initiazed

---
+++ ClothBazaar.Database/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using ClothBazar.Entities;//add from reference
+using System.Data.Entity;
+
+namespace ClothBazaar.Database
+{
+    class DBContext : DbContext
+    {
+        public DBContext():base("ClothBazarConnection")//tell to dbcontext about connectionstring name.
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+ 
---
+++ ClothBazar.Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+   public class Product : BaseEntity
+    {
+      
+        public Category Category { get; set; }//reference this product belong to caterory by make an obj
+        public decimal Price { get; set; }
+    }
+}
